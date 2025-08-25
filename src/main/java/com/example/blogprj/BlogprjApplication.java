6fa50846b8504205b88690d4dc8fdbd787cb2b4c
sAmH365 <npower5377@naver.com>
2025-08-25 14:06:02 +0900

    first commit

---
+++ src/main/java/com/example/blogprj/BlogprjApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogprjApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BlogprjApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/blogprj/BlogprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogprjApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

26fd995dc5b73f30c917e24555ff458ff4bb1de1
sAmH365 <npower5377@naver.com>
2025-09-01 10:59:23 +0900

    add final to ErrorResponse

--- src/main/java/com/example/blogprj/response/ErrorResponse.java
+++ src/main/java/com/example/blogprj/response/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.example.blogprj.response;
 
 
+import java.util.HashMap;
 import java.util.Map;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +12,7 @@ public class ErrorResponse {
 
   private final int code;
   private final String message;
-  private final Map<String, String> validation;
+  private final Map<String, String> validation = new HashMap<>();
 
   public void addValidation(String field, String errorMessage) {
     this.validation.put(field, errorMessage);
